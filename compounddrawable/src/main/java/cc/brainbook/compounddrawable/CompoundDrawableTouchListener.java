bfc345ba235b7168ad162615ee1a14ec46746368
Robert Han <brainbook.cc@outlook.com>
2020-09-02 15:50:23 -0700

    v1.0.0 AndroidX

--- compounddrawable/src/main/java/cc/brainbook/compounddrawable/CompoundDrawableTouchListener.java
+++ compounddrawable/src/main/java/cc/brainbook/compounddrawable/CompoundDrawableTouchListener.java
@@ -2,7 +2,7 @@ package cc.brainbook.compounddrawable;
 
 import android.graphics.Rect;
 import android.graphics.drawable.Drawable;
-import android.support.annotation.NonNull;
+import androidx.annotation.NonNull;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.TextView;
